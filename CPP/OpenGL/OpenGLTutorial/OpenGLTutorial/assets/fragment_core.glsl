9e471726ee87d3e3f61947a840dd5f3fa02e0193
Michael Grieco <michaelgriecocoding@gmail.com>
2020-02-17 17:14:28 -0500

    Episode 8

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/fragment_core.glsl
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/fragment_core.glsl
@@ -1,6 +1,10 @@
 #version 330 core
 out vec4 FragColor;
 
+// uniform vec4 ourColor; // set in code during render
+in vec3 ourColor;
+
 void main() {
-	FragColor = vec4(1.0f, 0.2f, 0.6f, 1.0f);
+	// FragColor = vec4(1.0f, 0.2f, 0.6f, 1.0f);
+	FragColor = vec4(ourColor, 1.0f);
 }
