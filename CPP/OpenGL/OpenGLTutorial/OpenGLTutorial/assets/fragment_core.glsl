81eb883124efa052dd48bf24449bb64ed88c6e5b
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-02-21 21:42:39 -0500

    Episode 9

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/fragment_core.glsl
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/fragment_core.glsl
@@ -3,8 +3,16 @@ out vec4 FragColor;
 
 // uniform vec4 ourColor; // set in code during render
 in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+uniform float mix;
 
 void main() {
-	// FragColor = vec4(1.0f, 0.2f, 0.6f, 1.0f);
-	FragColor = vec4(ourColor, 1.0f);
+	//FragColor = vec4(1.0f, 0.2f, 0.6f, 1.0f);
+	//FragColor = vec4(ourColor, 1.0f);
+	//FragColor = vec4(ourColor, 1.0f) * texture(texture1, TexCoord);
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mix);
 }
--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/main.cpp
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
 
 #include <fstream>
 #include <sstream>
@@ -14,7 +15,6 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-std::string loadShaderSrc(const char* filename);
 
 int main() {
 	int success;
@@ -56,11 +56,11 @@ int main() {
 	Shader shader("assets/vertex_core.glsl", "assets/fragment_core.glsl");
 
 	float vertices[] = {
-		// positions		// colors
-		-0.5f, -0.5f, 0.0f,	1.0f, 1.0f, 0.5f,	// bottom left
-		-0.5f, 0.5f, 0.0f,	0.5f, 1.0f, 0.75f,	// top left
-		0.5f, -0.5f, 0.0f,	0.6f, 1.0f, 0.2f,	// bottom right
-		0.5f, 0.5f, 0.0f,	1.0f, 0.2f, 1.0f	// top right
+		// positions		// colors			// texture coordinates
+		-0.5f, -0.5f, 0.0f,	1.0f, 1.0f, 0.5f,	0.0f, 0.0f,	// bottom left
+		-0.5f, 0.5f, 0.0f,	0.5f, 1.0f, 0.75f,	0.0f, 1.0f,	// top left
+		0.5f, -0.5f, 0.0f,	0.6f, 1.0f, 0.2f,	1.0f, 0.0f,	// bottom right
+		0.5f, 0.5f, 0.0f,	1.0f, 0.2f, 1.0f,	1.0f, 1.0f	// top right
 	};
 	unsigned int indices[] = {
 		0, 1, 2, // first triangle
@@ -86,17 +86,77 @@ int main() {
 
 	// set attributes pointers
 	// position
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	// color
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	// texture coordinate attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
-	glm::mat4 trans = glm::mat4(1.0f);
+	// TEXTURES_____________________________________
+
+	// generate texture
+	unsigned int texture1, texture2;
+
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	// image wrap (s, t, r) = (x, y, z)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// border color
+	float borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+	// image filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // scale up -> blend colors
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	// load image 1
+	int width, height, nChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load("assets/image1.jpg", &width, &height, &nChannels, 0);
+
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(data);
+
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	// load image 2
+	data = stbi_load("assets/image2.png", &width, &height, &nChannels, 0);
+
+	if (data) {
+		// RGBA because png
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(data);
+
+	shader.activate();
+	shader.setInt("texture1", 0);
+	shader.setInt("texture2", 1);
+
+	// transformation
+	/*glm::mat4 trans = glm::mat4(1.0f);
 	trans = glm::rotate(trans, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 	trans = glm::scale(trans, glm::vec3(0.5f, 1.5f, 0.5f));
 	shader.activate();
-	shader.setMat4("transform", trans);
+	shader.setMat4("transform", trans);*/
 
 	while (!glfwWindowShouldClose(window)) {
 		// process input
@@ -106,16 +166,22 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// bind texture
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+
 		// draw shapes
 		glBindVertexArray(VAO);
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
 		shader.activate();
 		// set color
-		float timeValue = glfwGetTime();
-		float blueValue = (sin(timeValue) / 2.0f) + 0.5f;
-		shader.set4Float("ourColor", 0.0f, 0.0f, blueValue, 1.0f);
-		trans = glm::rotate(trans, glm::radians(timeValue / 100), glm::vec3(0.1f, 0.1f, 0.1f));
-		shader.setMat4("transform", trans);
+		//float timeValue = glfwGetTime();
+		//float blueValue = (sin(timeValue) / 2.0f) + 0.5f;
+		//shader.set4Float("ourColor", 0.0f, 0.0f, blueValue, 1.0f);
+		//trans = glm::rotate(trans, glm::radians(timeValue / 100), glm::vec3(0.1f, 0.1f, 0.1f));
+		//shader.setMat4("transform", trans);
 
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//trans = glm::translate(trans, glm::vec3(0.5f, 0.5f, 0.0f));
