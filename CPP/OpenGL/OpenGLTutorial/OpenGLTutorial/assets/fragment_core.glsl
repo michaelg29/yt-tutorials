ae5d7181d09b9122b69064e87fae50a2b1ea3a3f
Michael Grieco <michaelgriecocoding@gmail.com>
2020-02-27 10:41:46 -0500

    Episode 10

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/fragment_core.glsl
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/fragment_core.glsl
@@ -8,11 +8,11 @@ in vec2 TexCoord;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
-uniform float mix;
+uniform float mixVal;
 
 void main() {
 	//FragColor = vec4(1.0f, 0.2f, 0.6f, 1.0f);
 	//FragColor = vec4(ourColor, 1.0f);
 	//FragColor = vec4(ourColor, 1.0f) * texture(texture1, TexCoord);
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mix);
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mixVal);
 }
