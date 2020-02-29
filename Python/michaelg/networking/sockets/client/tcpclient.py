0b9c5a65a2aa744f3f0dc35ac14680a3c66f8990
michaelg29 <michaelgriecocoding@gmail.com>
2020-02-29 13:04:15 -0500

    Python Networking - Episode 6

---
+++ Python/michaelg/networking/sockets/client/tcpclient.py
@@ -0,0 +1,26 @@
+import socket, threading
+
+class TcpClient:
+    def __init__(self, ipAddr, port, packetSize, hasInput=False):
+        pass
+
+    def run(self):
+        pass
+
+    def __serverThread(self):
+        pass
+
+    def inputThread(self):
+        pass
+
+    def send(self, msg, doEncode=True):
+        pass
+
+    def clientConnected(self):
+        pass
+
+    def serverDisconnected(self):
+        pass
+
+    def msgReceived(self, msg):
+        pass
