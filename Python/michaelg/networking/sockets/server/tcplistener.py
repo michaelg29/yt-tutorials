f6eca9b73b12de2f36361b417f73cc7b16d1592b
michaelg29 <michaelgriecocoding@gmail.com>
2020-02-29 13:00:41 -0500

    Python Networking - Episode 3

--- Python/michaelg/networking/sockets/server/tcplistener.py
+++ Python/michaelg/networking/sockets/server/tcplistener.py
@@ -25,8 +25,22 @@ class TcpListener:
         -------
         TcpServer object
         """
-        pass
+        
+        self.ipAddr = ipAddr
+        self.port = port
+        self.__server = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM
+        )
 
+        self.packetSize = packetSize
+        self.clients = []
+
+        self.__running = False
+        self.__binded = False
+        self.__hasCommands = hasCommands
+
+    # don't override
     def run(self):
         """
         public run function
@@ -38,8 +52,28 @@ class TcpListener:
         -------
         None
         """
-        pass
+        
+        # bind the socket to the ipAddr, port
+        self.__server.bind((self.ipAddr, self.port))
+        self.__binded = True
 
+        # initialize accept clients thread
+        self.__incomingConnectionThread = threading.Thread(
+            target=self.__acceptThread
+        )
+        self.__incomingConnectionThread.setDaemon(True)
+        self.__incomingConnectionThread.start()
+
+        # initialize the commands thread if necessary
+        if self.__hasCommands:
+            self.__commandsThread = threading.Thread(
+                target=self.cmdThread
+            )
+            self.__commandsThread.start()
+
+        self.__running = True
+
+    # don't override
     def __acceptThread(self):
         """
         private accept thread function
@@ -51,8 +85,26 @@ class TcpListener:
         ------
         None
         """
-        pass
+        
+        while self.__running:
+            # get client object
+            self.__server.listen(1)
 
+            clientSock, clientAddr = self.__server.accept()
+
+            client = self.generateClientObject(clientSock, clientAddr)
+            self.clients.append(client)
+            self.clientConnected(client)
+
+            # start client thread
+            clientThread = threading.Thread(
+                target=self.__clientThread,
+                args={client}
+            )
+            clientThread.setDaemon(True)
+            clientThread.start()
+
+    # don't override
     def __clientThread(self, client):
         """
         private client thread function
@@ -65,8 +117,38 @@ class TcpListener:
         ------
         None
         """
-        pass
+        
+        msg = ""
 
+        while True:
+            try:
+                while True:
+                    # get message
+                    data = client.sock.recv(self.packetSize).decode("latin1")
+                    if data[-8:] == "finished":
+                        # marks end of message
+                        msg += data[:-8]
+                        break
+                    msg += data
+                if not msg:
+                    break
+                elif msg.isspace():
+                    continue
+
+                self.msgReceived(client, msg)
+
+                msg = ""
+            except:
+                # error in communications, disconnect client
+                self.clientDisconnected(client)
+                self.clients.remove(client)
+                return
+
+        client.sock.close()
+        self.clientDisconnected(client)
+        self.clients.remove(client)
+
+    # override this
     def cmdThread(self):
         """
         command thread
