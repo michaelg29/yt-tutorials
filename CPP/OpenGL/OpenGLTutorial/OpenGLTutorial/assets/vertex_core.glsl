3a3f615d9c1901cf9f36df6ef43da2ddda4d8c18
Michael Grieco <michaelgriecocoding@gmail.com>
2020-03-13 10:33:26 -0400

    Episode 12

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/vertex_core.glsl
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/vertex_core.glsl
@@ -1,17 +1,22 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
+//layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aTexCoord;
 
-out vec3 ourColor; // output color to frag shader
+//out vec3 ourColor; // output color to frag shader
 out vec2 TexCoord;
 
 //uniform mat4 transform; // set in code
-uniform mat4 mouseTransform;
+//uniform mat4 mouseTransform;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main() {
 	//gl_Position = transform * vec4(aPos, 1.0);
-	gl_Position = mouseTransform * vec4(aPos, 1.0);
-	ourColor = aColor;
+	//gl_Position = mouseTransform * vec4(aPos, 1.0);
+	gl_Position = projection * view * model * vec4(aPos, 1.0);
+	//ourColor = aColor;
 	TexCoord = aTexCoord;
 }
