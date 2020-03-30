145236d518005309c61658ac63ab0b9a6df50ef2
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-03-30 01:25:42 -0400

    Episode - 6 - nav pt 3

--- Dart/Flutter/yt_tutorial/lib/page2.dart
+++ Dart/Flutter/yt_tutorial/lib/page2.dart
@@ -1,13 +1,17 @@
 import "package:flutter/material.dart";
 
 class Page2 extends StatelessWidget {
+  Map<String, dynamic> args;
+
+  Page2({this.args});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Page 2"),
       ),
-      body: Text("Page 2"),
+      body: Text(args["name"]),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.backspace),
         onPressed: () {
