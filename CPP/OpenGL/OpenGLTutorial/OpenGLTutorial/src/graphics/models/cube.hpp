6a40712cc24e5a7bfb1aa88f1feb53c66a2ed462
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-04-15 00:33:11 -0400

    Episode 16

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/graphics/models/cube.hpp
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/graphics/models/cube.hpp
@@ -2,17 +2,15 @@
 #define CUBE_H
 
 #include "../model.h"
-#include "../material.h"
+#include "../texture.h"
 
 class Cube : public Model {
 public:
 	glm::vec3 pos;
 	glm::vec3 size;
 
-	Material material;
-
-	Cube(Material material, glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f))
-		: material(material), pos(pos), size(size) {}
+	Cube(glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f))
+		: pos(pos), size(size) {}
 
 	void init() {
 		int noVertices = 36;
@@ -67,7 +65,12 @@ public:
 			indices[i] = i;
 		}
 
-		mesh = Mesh(Vertex::genList(vertices, noVertices), indices);
+        Texture tex("assets/flag.png", "material.diffuse");
+        tex.load();
+        Texture tex_specular("assets/flag_specular.png", "material.specular");
+        tex_specular.load();
+
+        mesh = Mesh(Vertex::genList(vertices, noVertices), indices, { tex, tex_specular });
 	}
 
 	void render(Shader shader) {
@@ -77,10 +80,9 @@ public:
 		//model = glm::rotate(model, (float)glfwGetTime() * glm::radians(-55.0f), glm::vec3(0.5f, 0.5f, 0.5f));
 		shader.setMat4("model", model);
 
-        shader.set3Float("material.ambient", material.ambient);
-        shader.set3Float("material.diffuse", material.diffuse);
-        shader.set3Float("material.specular", material.specular);
-        shader.setFloat("material.shininess", material.shininess);
+        mesh.textures[0].activate();
+        mesh.textures[1].activate();
+        shader.setFloat("material.shininess", 0.5f);
 
         Model::render(shader);
 	}
