d7556a9d2a943f6278009262bbbcba3b390059b8
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-08-05 21:54:49 -0400

    Updated gun model compatibility

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/graphics/models/gun.hpp
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/graphics/models/gun.hpp
@@ -4,6 +4,8 @@
 
 #include "../../scene.h"
 
+#include "../../physics/environment.h"
+
 class Gun : public Model {
 public:
     Gun(unsigned int maxNoInstances)
@@ -18,23 +20,23 @@ public:
 
         // set position
         // multiply offset by unit vector in 2 directions
-        rb.pos = scene->getActiveCamera()->cameraPos + glm::vec3(scene->getActiveCamera()->cameraFront * 0.5f) - glm::vec3(scene->getActiveCamera()->cameraUp * 0.205f);
-        model = glm::translate(model, rb.pos);
+        instances[0]->pos = scene->getActiveCamera()->cameraPos + glm::vec3(scene->getActiveCamera()->cameraFront * 0.5f) - glm::vec3(scene->getActiveCamera()->cameraUp * 0.205f);
+        model = glm::translate(model, instances[0]->pos);
 
         float theta;
 
         // rotate around camera right using dot product
-        theta = acos(glm::dot(scene->getActiveCamera()->worldUp, scene->getActiveCamera()->cameraFront) /
+        theta = acos(glm::dot(Environment::worldUp, scene->getActiveCamera()->cameraFront) /
             glm::length(scene->getActiveCamera()->cameraUp) / glm::length(scene->getActiveCamera()->cameraFront));
         model = glm::rotate(model, atanf(1) * 2 - theta, scene->getActiveCamera()->cameraRight); // offset by pi/2 radians bc angle btwn camFront and gunFront
 
         // rotate around cameraUp using dot product
         glm::vec2 front2d = glm::vec2(scene->getActiveCamera()->cameraFront.x, scene->getActiveCamera()->cameraFront.z);
         theta = acos(glm::dot(glm::vec2(1.0f, 0.0f), front2d) / glm::length(front2d));
-        model = glm::rotate(model, scene->getActiveCamera()->cameraFront.z < 0 ? theta : -theta, scene->getActiveCamera()->worldUp);
+        model = glm::rotate(model, scene->getActiveCamera()->cameraFront.z < 0 ? theta : -theta, Environment::worldUp);
 
         // scale
-        model = glm::scale(model, size);
+        model = glm::scale(model, instances[0]->size);
 
         shader.setMat4("model", model);
 
