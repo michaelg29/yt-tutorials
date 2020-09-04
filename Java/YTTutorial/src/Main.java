83285d54ed15ad4cb8693d1e62829cc4d14fc313
Michael Grieco <michaelgriecocoding@gmail.com>
2020-09-04 00:12:05 -0400

    Episode 4

--- Java/YTTutorial/src/Main.java
+++ Java/YTTutorial/src/Main.java
@@ -1,7 +1,44 @@
-import java.lang.System;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		// single line comments
+		/*
+		 * multi line comments
+		 */
+		
+		/*
+		 * variable declarations
+		 * int, double, float, char, byte, boolean, long
+		 */
+		
+		// type name = value;
+		int i = 1;
+		i = 3; // don't have to re-state type
+		
+		/*
+		 * expressions
+		 */
+		
+		// arithmetic operators: +, -, *, /, %
+		i = i + 3;
+		
+		// assignment operators: +=, -=, *=, /=, %=
+		i += 3; // i = i + 3
+		
+		// post-in/decrement operators
+		// returns current value then in/decrements
+		i++; // i += 1; i = i + 1
+		i--; // i -= 1
+		
+		i = 15;
+		System.out.println(3 * i++); // "45", i = 16
+		
+		// pre-in/decrement operators
+		// in/decrements then returns new value
+		++i;
+		--i;
+		
+		i = 15;
+		System.out.println(3 * --i); // "42", i = 14
 	}
 }
