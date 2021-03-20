19d915b5eaaf42d9750c7a2eed44293d5dc319f6
michaelg29 <michaelgriecocoding@gmail.com>
2021-03-19 20:54:19 -0400

    Reverted code to GLM

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/physics/collisionmesh.h
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/physics/collisionmesh.h
@@ -4,7 +4,6 @@
 #include <vector>
 
 #include "../algorithms/bounds.h"
-#include "../algorithms/cmathematics/vec.h"
 
 // forward declarations
 class CollisionModel;
@@ -14,8 +13,8 @@ typedef struct Face {
 	CollisionMesh* mesh;
 	unsigned int i1, i2, i3;
 
-	vec baseNormal;
-	vec norm;
+	glm::vec3 baseNormal;
+	glm::vec3 norm;
 
 	bool collidesWith(struct Face& face);
 } Face;
@@ -25,7 +24,7 @@ public:
 	CollisionModel* model;
 	BoundingRegion br;
 
-	std::vector<vec> points;
+	std::vector<glm::vec3> points;
 	std::vector<Face> faces;
 
 	CollisionMesh(unsigned int noPoints, float* coordinates, unsigned int noFaces, unsigned int* indices);
