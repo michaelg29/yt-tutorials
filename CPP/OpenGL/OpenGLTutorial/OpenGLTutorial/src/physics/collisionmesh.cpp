8b182c5980526cf8334a418689f303a49e2bf5e3
michaelg29 <michaelgriecocoding@gmail.com>
2022-02-16 13:05:25 -0500

    Moved Linear Algebra file

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/physics/collisionmesh.cpp
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/physics/collisionmesh.cpp
@@ -2,7 +2,7 @@
 #include "collisionmodel.h"
 #include "rigidbody.h"
 
-#include "../linalg.h"
+#include "../algorithms/math/linalg.h"
 
 bool Face::collidesWithFace(RigidBody* thisRB, Face& face, RigidBody* faceRB, glm::vec3& retNorm) {
 	// transform coordinates so that P1 is the origin
